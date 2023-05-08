12df66a9a1e32eb17b791f33eb3f6be6a6203a81
AnyStepanyan <a.stepaniann@gmail.com>
2023-05-08 23:37:19 +0400

    feat:add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const path = require('path')
+const fs = require('fs')
+const { stdout } = process
 
+
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, output => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, output => {
+        stdout.write("The folder 'files-copy' was created")
+    })
+
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (output, files) => {
+        for (let file of files) {
+
+            fs.copyFile(path.join(path.join(__dirname, 'files'), file.name), path.join(path.join(__dirname, 'files-copy'), file.name), output => {
+                if (output) stdout.write(output);
+            })
+
+        }
+    })
+})
